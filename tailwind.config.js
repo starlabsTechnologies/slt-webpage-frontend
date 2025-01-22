0cf0564fafbcd93471fca534493d08211b3d8b86
Aniket-Starlabs <aniket.ra@starlabs.co.in>
2025-01-22 19:08:18 +0530

    Assignment Form Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
       screens: {
         xs: "410px",
       },
